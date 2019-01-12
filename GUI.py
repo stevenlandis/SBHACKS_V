7213191cdaea020b90fa266f093e4b7485977f99
Christopher Lin <christopherlin@umail.ucsb.edu>
2019-01-12 13:23:18 -0800

    GUI.py
    Progress

--- GUI.py
+++ GUI.py
@@ -10,8 +10,14 @@ from tkinter import *
 from GPIOLibrary import GPIOProcessor
 import time
 
-top = tkinter.Tk()
-label = tkinter.Label( top, text='Click on the buttons to see the magic', relief=RAISED )
+
+
+
+top = Tk()
+top.title("Welcome to this demo")
+label = Label( top, text='Click on the buttons to see the magic', relief=RAISED )
+
+top.iconbitmap('favicon.ico')
 
 def helloCallBack():
    messagebox.showinfo( "Hello Python", "Hello World")
@@ -25,17 +31,20 @@ def temperatureCallBack():
 def LEDCallBack():
    GP = GPIOProcessor()
    try:
-      Pin27 = GP.getPin27()
-      Pin27.out()
-      Pin29 = GP.getPin29()
-      Pin29.input()
-      for i in range(0,20):
+    Pin27 = GP.getPin27()
+    Pin27.out()
+
+    Pin29 = GP.getPin29()
+    Pin29.input()
+
+    for i in range(0,20):
          pinValue = Pin29.getValue();
          if pinValue == 1:
             Pin27.high()
          else:
             Pin27.low()
             time.sleep(1)
+
    finally:
       GP.cleanup()
    #messagebox.showinfo( "Hello Python", "Hello World")
@@ -43,11 +52,11 @@ def LEDCallBack():
 def turnNobCallBack():
    messagebox.showinfo( "Hello Python", "Hello World")
 
-helloButton = tkinter.Button(top, text ="Hello", command = helloCallBack)
-touchButton = tkinter.Button(top, text ="Touch Sensor", command = touchCallBack)
-temperatureButton = tkinter.Button(top, text ="Temperature Sensor", command = temperatureCallBack)
-LEDButton = tkinter.Button(top, text ="LED", command = LEDCallBack)
-turnNobButton = tkinter.Button(top, text ="Turn Nob", command = turnNobCallBack)
+helloButton = Button(top, text ="Hello", command = helloCallBack)
+touchButton = Button(top, text ="Touch Sensor", command = touchCallBack)
+temperatureButton = Button(top, text ="Temperature Sensor", command = temperatureCallBack)
+LEDButton = Button(top, text ="LED", command = LEDCallBack)
+turnNobButton = Button(top, text ="Turn Nob", command = turnNobCallBack)
 
 label.pack()
 helloButton.pack()
