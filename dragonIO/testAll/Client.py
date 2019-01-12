90cf7937302f82b2b496316d0e7c2a9ad1dddb88
stevenlandis <stevenlandis@comcast.net>
2019-01-12 15:09:38 -0800

    Update Client.py

--- dragonIO/testAll/Client.py
+++ dragonIO/testAll/Client.py
@@ -27,7 +27,7 @@ def on_open(ws):
 
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    ws = websocket.WebSocketApp("ws://localhost:" + port + "/",
+    ws = websocket.WebSocketApp("ws://169.231.141.17:22/",
                               on_message = on_message,
                               on_error = on_error,
                               on_close = on_close)
