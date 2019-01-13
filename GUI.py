af5bc64b0a254af50b04b6f28e3a9f90bf05908d
Christopher Lin <christopherlin@umail.ucsb.edu>
2019-01-12 18:57:31 -0800

    updated GUI.py

--- GUI.py
+++ GUI.py
@@ -1,6 +1,11 @@
 from tkinter import *
 
 rotItem = ''
+touchItem = ''
+lightItem = ''
+buttonActItem = ''
+clickItem = ''
+encoderItem = ''
 
 
 tk = Tk()
@@ -23,6 +28,16 @@ encoderVar.set("one")
 
 def getRotItem(self):
    rotItem = rotVar.get()
+def getTouchItem(self):
+   touchItem = touchVar.get()
+def getLightItem(self):
+   lightItem = lightVar.get()
+def getbuttonActItem(self):
+   buttonActItem = buttonActVar.get()
+def getClickItem(self):
+   clickItem = clickVar.get()
+def getEncoderItem(self):
+   encoderItem = encoderVar.get()
 
 
 
@@ -34,11 +49,11 @@ Label(tk, text="Click:").grid(column=0,row=4)
 Label(tk, text="Encoder:").grid(column=0,row=5)
 
 rot = OptionMenu(tk, rotVar, "one", "two", "three", command=getRotItem).grid(column=1,row=0)
-touch = OptionMenu(tk, touchVar, "one", "two", "three").grid(column=1,row=1)
-light = OptionMenu(tk, lightVar, "one", "two", "three").grid(column=1,row=2)
-buttonAct = OptionMenu(tk, buttonActVar, "one", "two", "three").grid(column=1,row=3)
-click = OptionMenu(tk, clickVar, "one", "two", "three").grid(column=1,row=4)
-encoder = OptionMenu(tk, encoderVar, "one", "two", "three").grid(column=1,row=5)
+touch = OptionMenu(tk, touchVar, "one", "two", "three", command=getTouchItem).grid(column=1,row=1)
+light = OptionMenu(tk, lightVar, "one", "two", "three", command=getLightItem).grid(column=1,row=2)
+buttonAct = OptionMenu(tk, buttonActVar, "one", "two", "three", command=getbuttonActItem).grid(column=1,row=3)
+click = OptionMenu(tk, clickVar, "one", "two", "three", command=getClickItem).grid(column=1,row=4)
+encoder = OptionMenu(tk, encoderVar, "one", "two", "three", command=getEncoderItem).grid(column=1,row=5)
 
 
 button = Button(tk, text="OK", command=getRotItem)
