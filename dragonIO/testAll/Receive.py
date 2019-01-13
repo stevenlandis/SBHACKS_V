ce328186a5b9d9e8958d180b4624ec0eda45adff
gareyflee <46618024+gareyflee@users.noreply.github.com>
2019-01-12 16:03:10 -0800

    Lots of stuff...

--- dragonIO/testAll/Receive.py
+++ dragonIO/testAll/Receive.py
@@ -1,13 +1,24 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+import Keyboard_Control as keyboard
+import Volume_Control as volume
 
 import sys
+def Parse_String_From_Board(string):
+    if "rot" in string:
+        turnVal = round(int(string[4:])/1023*100)
+        volume.Set_Volume(turnVal)
+    elif "button" in string:
+        keyboard.Tab()
 
-port = int(sys.argv[1])
+
+#port = int(sys.argv[1])
+port = 22
 
 class SimpleEcho(WebSocket):
 
     def handleMessage(self):
         # echo message back to client
+        Parse_String_From_Board(self.data)
         self.sendMessage(self.data)
 
     def handleConnected(self):
