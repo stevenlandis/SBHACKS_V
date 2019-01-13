86c905c9bcb0f2dd587112f4c0c754c69738d306
mknguyen1202 <mknguyen1202@gmail.com>
2019-01-12 16:56:29 -0800

    callFunction

--- GUI.py
+++ GUI.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
+
+
 # Code to add widgets will go here...
+
 
 import tkinter
 from tkinter import messagebox
@@ -7,26 +10,59 @@ from tkinter import *
 from GPIOLibrary import GPIOProcessor
 import time
 
+
+#global theme
+theme = 0
+
 top = Tk()
 top.title("Welcome to this demo")
 top.geometry("600x400+300+200")
-top.config(background = "white")
-
+top.config(background = "black")
 top.resizable(0,0)
 label = Label( top, text='Click on the buttons to see the magic', bg = "white")
-
 #top.iconbitmap('favicon.ico')
+
+
+class HoverBinding:
+   def __init__(self, button):
+      self.button = button
+   def on_enter(self, e):
+       self.button['background'] = 'grey'
+
+   def on_leave(self, e):
+       self.button['background'] = 'SystemButtonFace'
+
+   def enterLeaveBinding(self):
+      print("in Enter and Leave")
+      self.button.bind("<Enter>", self.on_enter)
+      self.button.bind("<Leave>", self.on_leave)
+
+def callFunction(string):
+   func = string.split(':')
+   if (len(func) < 2):
+      print("String sent has errors!")
+      return
+   if (func[0] == "touch"):
+      touchcallBack(func[1])
+   elif (func[0] == "temperature"):
+      temperatureCallBack(func[1])
+   elif (func[0] == "LED"):
+      LEDCallBack(func[1])
+   elif (func[0] == "turnNob"):
+      turnNobCallBack(func[1])
+   elif (func[0] == "light"):
+      lightCallBack(func[1])
 
 def helloCallBack():
    messagebox.showinfo( "Hello Python", "Hello World")
 
-def touchCallBack():
+def touchCallBack(data):
    messagebox.showinfo( "Hello Python", "Hello World")
 
-def temperatureCallBack():
+def temperatureCallBack(data):
    messagebox.showinfo( "Hello Python", "Hello World")
 
-def LEDCallBack():
+def LEDCallBack(data):
    GP = GPIOProcessor()
    try:
     Pin27 = GP.getPin27()
@@ -47,22 +83,38 @@ def LEDCallBack():
       GP.cleanup()
    #messagebox.showinfo( "Hello Python", "Hello World")
 
-def turnNobCallBack():
+def turnNobCallBack(data):
    messagebox.showinfo( "Hello Python", "Hello World")
+
+
+def changeBackgroundColor():
+   global theme
+   if (theme % 2 ==0):
+      theme+=1
+      top.config(background = "white")
+      return
+   theme+=1
+   top.config(background = "black")
 
 helloButton = Button(top, text ="            Hello              ",fg = "red", command = helloCallBack)
 touchButton = Button(top, text ="     Touch Sensor       ", fg = "orange", command = touchCallBack)
 temperatureButton = Button(top, text ="Temperature Sensor", fg="green", command = temperatureCallBack)
 LEDButton = Button(top, text ="               LED              ", fg="blue",command = LEDCallBack)
 turnNobButton = Button(top, text ="          Turn Nob         ", fg="indigo", command = turnNobCallBack)
+quitbutton = Button(top, text = "               Exit               ", fg="violet", command = quit)
+themeButton = Button(top, text = "             Theme              ", fg="turquoise", command = changeBackgroundColor)
 
-def quitter(self):
-   top.destroy()
-
-
-top.bind('q', quitter)
-top.bind('<Escape>', quitter)
-
+buttonList = []
+buttonList.append(HoverBinding(helloButton))
+buttonList.append(HoverBinding(touchButton))
+buttonList.append(HoverBinding(temperatureButton))
+buttonList.append(HoverBinding(LEDButton))
+buttonList.append(HoverBinding(turnNobButton))
+buttonList.append(HoverBinding(quitbutton))
+buttonList.append(HoverBinding(themeButton))
+for item in buttonList:
+   print("something something")
+   item.enterLeaveBinding()
 
 
 label.pack()
@@ -71,4 +123,7 @@ touchButton.pack()
 temperatureButton.pack()
 LEDButton.pack()
 turnNobButton.pack()
+quitbutton.pack()
+themeButton.pack()
+
 top.mainloop()
